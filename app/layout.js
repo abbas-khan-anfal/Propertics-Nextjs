3e6af9855030e4d646621147890769f5807f82f5
Abbas Khan <ak9950025@gmail.com>
2025-06-03 12:55:39 +0500

    fix: use next/script for bootstrap

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import GlobalProvider from "./context/globalProvider";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: 'Propertics | Affordable Properties in Pakistan',
@@ -22,15 +23,19 @@ export default function RootLayout({ children }) {
 
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossOrigin="anonymous"></link>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossOrigin="anonymous"></script>
-
-
       </head>
       <body>
         <GlobalProvider>
           <Toaster/>
           {children}
         </GlobalProvider>
+
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
